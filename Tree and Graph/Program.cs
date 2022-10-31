a74eb558de60ba15dc95da3c695386a672c24243
nithyaa-pt6485 <nithyaa-pt6485@NITHYAA-PT6485>
2022-10-31 16:37:20 +0530

    Add project files.

---
+++ Tree and Graph/Program.cs
@@ -0,0 +1,74 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        UndirectedSimpleGraphExample();
+        
+        BinaryTreeExample();
+               
+    }
+
+    public static void BinaryTreeExample()
+    {
+        Console.Clear();
+        BinaryTree binaryTree = new BinaryTree();
+
+        binaryTree.Insert(1);
+        binaryTree.Insert(2);
+        binaryTree.Insert(7);
+        binaryTree.Insert(3);
+        binaryTree.Insert(10);
+        binaryTree.Insert(5);
+        binaryTree.Insert(8);
+
+        Node node = binaryTree.Find(5);
+        int depth = binaryTree.GetTreeDepth();
+
+        Console.WriteLine("PreOrder Traversal:");
+        binaryTree.TraversePreOrder(binaryTree.Root);
+        Console.WriteLine();
+
+        Console.WriteLine("InOrder Traversal:");
+        binaryTree.TraverseInOrder(binaryTree.Root);
+        Console.WriteLine();
+
+        Console.WriteLine("PostOrder Traversal:");
+        binaryTree.TraversePostOrder(binaryTree.Root);
+        Console.WriteLine();
+
+        binaryTree.Remove(7);
+        binaryTree.Remove(8);
+
+        Console.WriteLine("PreOrder Traversal After Removing Operation:");
+        binaryTree.TraversePreOrder(binaryTree.Root);
+        Console.WriteLine();
+
+        Console.ReadLine();
+    }
+
+    public static void UndirectedSimpleGraphExample()
+    {
+        UndirectedSimpleGraph<string> graph = new UndirectedSimpleGraph<string>(3);
+        graph.addNode("city1");
+        graph.addNode("city2");
+        graph.addNode("city3");
+
+        graph.addEdge("city1", "city2");
+        Console.WriteLine(graph.areAdjacents("city1", "city2"));
+        graph.removeEdge("city1", "city2");
+        Console.WriteLine(graph.areAdjacents("city1", "city2"));
+        graph.addEdge("city1", "city2");
+        graph.addEdge("city1", "city3");
+        List<String> adjacents = graph.adjacentsTo("city1");
+        Console.WriteLine("city1 adjacents count: " + adjacents.Count);
+        foreach (string city in adjacents)
+        {
+            Console.WriteLine(city);
+        }
+
+        Console.ReadLine();
+    }
+}
+
+
+
---
+++ Tree and Graph/UndirectedSimpleGraph.cs
@@ -0,0 +1,94 @@
+﻿public class UndirectedSimpleGraph<T> 
+{
+    List<T> nodes;
+    bool[,] adjacencyMatrix;
+
+    public UndirectedSimpleGraph(int maxNumberOfNodes)
+    {
+        nodes = new List<T>(maxNumberOfNodes);
+        adjacencyMatrix = new bool[maxNumberOfNodes, maxNumberOfNodes];
+        for (int i = 0; i < maxNumberOfNodes; i++)
+        {
+            for (int j = 0; j < maxNumberOfNodes; j++)
+            {
+                adjacencyMatrix[i, j] = false;
+            }
+        }
+    }
+
+    public void addEdge(T node1, T node2)
+    {
+        validateNodes(node1, node2);
+        setEdge(node1, node2, true);
+    }
+
+    public void removeEdge(T node1, T node2)
+    {
+        validateNodes(node1, node2);
+        setEdge(node1, node2, false);
+    }
+
+    public void addNode(T node)
+    {
+        nodes.Add(node);
+    }
+
+    public void removeNode(T node)
+    {
+        nodes.Remove(node);
+    }
+    public List<T> getNodes()
+    {
+        return nodes;
+    }
+
+
+    public bool areAdjacents(T node1, T node2)
+    {
+        validateNodes(node1, node2);
+        int row = nodes.IndexOf(node1);
+        int col = nodes.IndexOf(node2);
+        return adjacencyMatrix[row, col];
+    }
+
+    public List<T> adjacentsTo(T node)
+    {
+        if (!nodes.Contains(node))
+        {
+            Console.WriteLine("The node is not on the graph");
+            return null;
+        }
+        List<T> adjacents = new List<T>();
+        int row = nodes.IndexOf(node);
+        for (int i = 0; i < nodes.Count(); i++)
+        {
+            if (adjacencyMatrix[row, i])
+            {
+                adjacents.Add(nodes[i]);
+            }
+        }
+        return adjacents;
+    }
+
+    private void validateNodes(T node1, T node2)
+    {
+        if (node1.Equals(node2)) 
+            Console.WriteLine("Simple graphs cannot have loops");
+        
+        else if (!nodes.Contains(node1) || !nodes.Contains(node2))
+        {
+            throw new Exception("One of the nodes is not on the graph");
+        }
+    }
+
+    private void setEdge(T node1, T node2, bool value)
+    {
+        int row = nodes.IndexOf(node1);
+        int col = nodes.IndexOf(node2);
+        adjacencyMatrix[row, col] = value;
+        adjacencyMatrix[col, row] = value;
+    }
+}
+
+
+
